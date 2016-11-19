5d9950e1e2092f64b04750dbaa33f00bcb05743c
RafaLunaP <valtos1985@hotmail.com>
2016-11-18 21:30:04 -0600

    Agregado puesto y cuadro de citas

---
+++ js/citas.js
@@ -0,0 +1,114 @@
+// JavaScript Document
+		var autor=new Array() 
+		
+		autor[0] = "El diseño es un plan para ordenar elementos de la mejor manera posible para llevar a cabo un propósito particular. -Charles Eames-";
+		
+		autor[1] = "Todo está diseñado. Pocas cosas están bien diseñadas. -Brian Reed-";
+		
+		autor[2] = "No hay diseño sin disciplina. No hay disciplina sin inteligencia.  -Massimo Vignelli-";
+
+		autor[3] = "La gente ignora el diseño que ignora a la gente. -Frank Chimero-";
+
+		autor[4] = "El buen diseño es obvio. El diseño genial es transparente. -Joe Sparano-";
+
+		autor[5] = "Diseño es donde la ciencia y el arte llegan a un punto de equilibrio. -Robin Mathew-";
+
+		autor[6] = "El buen diseño va al cielo, el mal diseño a todos los sitios. -Mieke Gerritzen-";
+
+		autor[7] = "Un diseñador es un planificador con sentido del buen gusto. -Bruno Munari-";
+
+		autor[8] = "El diseño nunca debería de decir “Mírame”. Debería de decir: “Mira esto”. -David Craib-";
+
+		autor[9] = "El mal diseño es humo, mientras que el buen diseño es un espejo. -Juan Carlos Fernàndez-";
+
+		autor[10] = "No diseñes para todo el mundo. Es imposible. Todo lo que acabas haciendo es diseñar algo que hace a todo el mundo infeliz. -Leisa Reichelt-";
+
+		autor[11] = "El proceso de diseño más óptimo integra las aspiraciones del arte, la ciencia y la cultura. -Jeff Smith-";
+
+		autor[12] = "El buen diseño es una suerte de clarividencia hecha visual. -Edward Tufte-";
+
+		autor[13] = "El diseño es inteligencia hecha visible. -Alina Wheeler-";
+
+		autor[14] = "La Matemática es fácil; el diseño es duro. -Jeffrey Veen-";
+
+		autor[15] = "El diseño es el esfuerzo consciente de imponer un orden significativo. -Victor Papanek-";
+
+		autor[16] = "El contenido antecede al diseño. El diseño en ausencia de contenido no es diseño, es decoración. -Jeffrey Zeldman-";
+
+		autor[17] = "Los diseñadores tienen una doble obligación; contractualmente con sus clientes y moralmente con los usuarios y destinatarios finales de su trabajo. -Hans Höger-"; 
+
+		autor[18] = "Los ordenadores son al diseño lo que el microondas a la cocina. -Milton Glaser-"; 
+
+		autor[19] = "Tecnología sobre técnica produce diseño sin emoción. -Daniel Mall-";
+
+		autor[20] = "Un diseño no está terminado hasta que alguien lo está utilizando. -Brenda Laurel-";
+
+		autor[21] = "La vida de un diseñador es una vida de lucha: lucha contra la fealdad. -Massimo Vignelli-";
+
+		autor[22] = "Si el diseño no es rentable, entonces es arte. -Henrik Fiskar-";
+
+		autor[23] = "Practica un diseño seguro: Usa un concepto. -Petrula Vrontikis-";
+
+		autor[24] = "El diseño realmente elegante incorpora una funcionalidad de primera clase en una forma simple y ordenada. -David Lewis-";
+
+		autor[25] = "Un diseñador sabe que ha logrado la perfección no cuando no hay nada que añadir, sino  cuando no hay nada que quitar. -Antoine de Saint Exupéry-";
+
+		autor[26] = "Es realmente difícil diseñar productos por grupos de interés. Muchas veces, las personas no saben lo que quieren hasta que se lo muestras. -Steve Jobs-";
+
+		autor[27] = "Ningún diseño funciona a no ser que incorporen las ideas que tienen en común las personas a las que se dirige el objeto que se quiere diseñar. -Adrian Forty-";
+
+		autor[28] = "Muchas cosas difíciles de diseñar muestran ser fáciles de realizar. -Samuel Johnson-";
+
+		autor[29] = "Lo único importante del diseño es como se relaciona con la gente. -Victor Papanek-";
+
+		autor[30] = "Diseñar un producto es diseñar una relación. -Steve Rogers-";
+
+		autor[31] = "Un gran diseño no venderá un producto de poca calidad, pero permitirá a un gran producto alcanzar su máximo potencial. -Thomas J. Watson Jr.-";
+
+		autor[32] = "El diseño es el embajador silencioso de tu marca. -Paul Rand-"; 
+
+		autor[33] = "Piensa más, diseña menos. -Ellen Lupton-"; 
+
+		autor[34] = "El diseño es el intermediario entre la información y la comprensión. -Richard Grefé-"; 
+
+		autor[35] = "El diseño no es sólo lo que se ve y se siente. El diseño es cómo funciona. -Steve Jobs-"; 
+
+		autor[36] = "El diseño crea cultura. Cultura determina los valores. Los valores determinan el futuro. -Robert L. Peters-"; 
+
+		autor[37] = "Diseño no es lo que ves, sino lo que debes hacer que otras personas vean. -Edgar Degas-";
+
+		autor[38] = "La inspiración es para los aficionados. El resto de nosotros sólo la conseguimos trabajando. -Chuck Close-";
+
+		autor[39] = "No eres un diseñador hasta que no sabes unos cuantos trucos, pero no eres un buen diseñador hasta que no sabes cuando no utilizarlos. -Kent Beck-";
+
+		autor[40] = "Es arte si no se puede explicar. Es moda si nadie pide una explicación. Es diseño si no necesita explicación. -Wouter Stokkel-";
+
+		autor[41] = "Limítate a usar dos tipografías para que los lectores se centren en las palabras, no en cuántas fuentes se usan para expresarlas. -Dan Rubin-";
+
+		autor[42] = "El diseño gráfico es simple, por eso es tan complicado. -Paul Rand-"; 
+
+		autor[43] = "El diseño se trata de la necesidad. -Charles Eames-";
+
+		autor[44] = "Todo es diseño, ¡todo!. -Paul Rand-";
+
+		autor[45] = "El diseño gráfico debe seducir, educar y, quizás lo más importante, provocar una respuesta emocional. -April Greiman-";
+
+		autor[46] = "Reconocer la necesidad es la principal condición para el diseño. -Charles Eames-";
+
+		autor[47] = "Un buen diseño puede soportar la moda de 10 años. -Yves Saint Laurent-";
+
+		autor[48] = "Una sola palabra traiciona a menudo un gran diseño. -Jean Baptiste Racine-";
+
+		autor[49] = "Es más rápido despreciar un diseño, señalando todos sus fallos, que hacer algo genial por uno mismo. -Tom Watson-";
+
+		autor[50] = "Los clientes están llenos de sorpresas. El diseño es cómo aprovechas esas sorpresas. -Dan Philips-";
+
+		autor[51] = "El diseño es una respuesta formal a una pregunta estratégica. -Mariona López-";
+
+
+		//Esto es solo un ejemplo de como usar random
+		//var whichquote = Math.floor(Math.random()*(autor.length))
+
+		var now = new Date ();
+
+		var day = now.getDate();		
