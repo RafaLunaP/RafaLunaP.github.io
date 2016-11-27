668eebc884b246d0f6216e8321c1ec68b2c5ad39
RafaLunaP <valtos1985@hotmail.com>
2016-11-27 11:41:53 -0600

    Eliminación de código basura en galeria.js y reacomodo de conflictos internos en smoothNav.js

--- js/smoothNav.js
+++ js/smoothNav.js
@@ -1,1 +1,14 @@
-$(function(){$('a[href*=#]').click(function(){if(location.pathname.replace(/^\//,'')==this.pathname.replace(/^\//,'')&&location.hostname==this.hostname){var $target=$(this.hash);$target=$target.length&&$target||$('[name='+this.hash.slice(1)+']');if($target.length){var targetOffset=$target.offset().top;$('html,body').animate({scrollTop:targetOffset},1000);return false;}}});});;(function($){'use strict';var $body=$('html, faq'),content=$('#main').smoothState({onStart:{duration:2500,render:function(url,$container){content.toggleAnimationClass('is-exiting');$body.animate({scrollTop:0});}}}).data('smoothState');})(jQuery);
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
